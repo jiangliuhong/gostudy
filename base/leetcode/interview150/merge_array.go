08bab32353d08a3fa0c0e0503155b0bebaf202a4
jiangliuhong <ja_rome@163.com>
2023-09-18 17:43:21 +0800

    update

---
+++ base/leetcode/interview150/merge_array.go
@@ -0,0 +1,10 @@
+package interview150
+
+import "sort"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for index, item := range nums2 {
+		nums1[index+m] = item
+	}
+	sort.Ints(nums1)
+}
--- base/leetcode/interview150/remove_duplicates.go
+++ base/leetcode/interview150/remove_duplicates.go
@@ -1,5 +1,23 @@
 package interview150
 
+// 26. 删除有序数组中的重复项
+// 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，
+// 返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+func removeDuplicates26(nums []int) int {
+	if len(nums) == 1 {
+		return 1
+	}
+	left, right := 0, 1
+	for right < len(nums) {
+		if nums[left] != nums[right] {
+			left++
+			nums[left] = nums[right]
+		}
+		right++
+	}
+	return left + 1
+}
+
 // 80. 删除有序数组中的重复项 II
 // 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
 // 快慢指针写法
