1a3ade04b4d6f075b67179cc5901a6c21ef9c071
jiangliuhong <ja_rome@163.com>
2023-09-26 15:42:33 +0800

    update

---
+++ base/leetcode/interview150/h_index.go
@@ -0,0 +1,20 @@
+package interview150
+
+import "sort"
+
+// 274. H 指数
+// 给你一个整数数组 citations ，其中 citations[i] 表示研究者的第 i 篇论文被引用的次数。计算并返回该研究者的 h 指数。
+func hIndex(citations []int) int {
+	sort.Ints(citations)
+	size := len(citations)
+	for i := 0; i < size; i++ {
+		if size-i <= citations[i] {
+			return size - i
+		}
+	}
+	return 0
+}
+
+// 5
+// 0,1,3,6,5
+// 0,1,2,3,4
