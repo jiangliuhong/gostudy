dc64409a2a4f9e9d5a4eb881d9406deddd14ab5b
jiangliuhong <ja_rome@163.com>
2023-10-31 10:26:09 +0800

    add int to roman

--- base/leetcode/interview150/arraystring/int_to_roman.go
+++ base/leetcode/interview150/arraystring/int_to_roman.go
@@ -2,5 +2,17 @@ package arraystring
 
 // 12. 整数转罗马数字
 func intToRoman(num int) string {
-
+	dictKey := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	dictVal := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	roma := ""
+	for i := 0; i < len(dictKey); {
+		key := dictKey[i]
+		if num >= key {
+			num -= key
+			roma += dictVal[i]
+		} else {
+			i++
+		}
+	}
+	return roma
 }
