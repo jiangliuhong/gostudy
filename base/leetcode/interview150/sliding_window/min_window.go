fdf9278c0eb7ec4d308c67b4bafa897609820459
jiangliuhong <ja_rome@163.com>
2023-12-14 11:51:36 +0800

    完成最小覆盖子串

--- base/leetcode/interview150/sliding_window/min_window.go
+++ base/leetcode/interview150/sliding_window/min_window.go
@@ -2,17 +2,45 @@ package sliding_window
 
 // 76. 最小覆盖子串
 func minWindow(s string, t string) string {
-	ls, lt := len(s), len(t)
-	if lt > ls {
-		return ""
+	ls, left, right, res := len(s), 0, 0, ""
+	dictV, dictC := map[rune]int{}, map[rune]bool{}
+	for _, item := range t {
+		dictV[item]++
+		dictC[item] = true
 	}
-	td := map[rune]int{}
-	for i := 0; i < lt; i++ {
-		td[rune(t[i])]++
+	for left <= right && right <= ls {
+		if isZero(dictV) {
+			if res == "" {
+				res = s[left:right]
+			} else {
+				resT := s[left:right]
+				if len(resT) < len(res) {
+					res = resT
+				}
+			}
+			itemS := rune(s[left])
+			if _, ok := dictC[itemS]; ok {
+				dictV[itemS]++
+			}
+			left++
+		} else if right == ls {
+			break
+		} else {
+			itemS := rune(s[right])
+			if _, ok := dictC[itemS]; ok {
+				dictV[itemS]--
+			}
+			right++
+		}
 	}
-	temp := map[rune]int{}
-	for i := 0; i < ls; i++ {
-		temp[rune(s[i])]++
+	return res
+}
+
+func isZero(m map[rune]int) bool {
+	for _, v := range m {
+		if v > 0 {
+			return false
+		}
 	}
-	return ""
+	return true
 }
---
+++ base/leetcode/interview150/sliding_window/min_window_test.go
@@ -0,0 +1,9 @@
+package sliding_window
+
+import (
+	"testing"
+)
+
+func TestMinWinodow(t *testing.T) {
+	t.Logf(minWindow("ADOBECODEBANC", "ABC"))
+}
